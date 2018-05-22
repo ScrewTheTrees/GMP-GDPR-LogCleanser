7564d27405d89c706c9e3ef3e4191592db6ad948
Fredrik Grimmenhag <fredrik.grimmenhag@gmp-systems.se>
2018-05-22 13:08:12 +0200

    Cleanup //! 2

--- src/main/java/com/gmpsystems/logcleaner/Services/DirectoryService.java
+++ src/main/java/com/gmpsystems/logcleaner/Services/DirectoryService.java
@@ -1,8 +1,9 @@
 package com.gmpsystems.logcleaner.Services;
 
 import java.io.*;
-import java.nio.channels.FileChannel;
-import java.nio.file.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
